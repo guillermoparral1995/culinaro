b215b93506abbe85f51b830a251fb3849efe7a59
Guillermo Parral <guillermoeparral@gmail.com>
2024-05-09 10:03:39 +0200

    remove dangling import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from "@astrojs/node";
 import auth from "auth-astro";
 
 import netlify from "@astrojs/netlify";
